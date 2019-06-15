f55bfa45062f5bf4903345660014d0032a371726
wqh <wqh@nxin.com>
2019-06-15 10:17:35 +0800

    123456

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}hllow</h1>
+    <h1>{{ msg }}123456</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
